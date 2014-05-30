17dc0ad6ad27269a5f0e128b333481b78e79dbe8
Alexandre Quessy <alexandre@quessy.net>
2014-05-30 18:02:58 -0400

    osx-specific run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,14 @@
 #export GST_PLUGIN_PATH=/Library/Frameworks/GStreamer.framework/Libraries
 #export GST_DEBUG=2
 export LANG=C
-./mapmap.app/Contents/MacOS/mapmap
+
+unamestr=$(uname)
+
+if [[ $unamestr == "Darwin" ]]; then
+    export DYLD_PRINT_LIBRARIES=1
+    #lldb ./MapMap.app/Contents/MacOS/MapMap
+    ./MapMap.app/Contents/MacOS/MapMap
+elif [[ $unamestr == "Linux" ]]; then
+    ./mapmap
+fi
 
